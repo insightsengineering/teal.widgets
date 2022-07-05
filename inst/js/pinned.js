8fe6f9554e298c943d3039c0697704d0def7fbba
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-07-05 09:27:40 +0200

    Delete pinned.js

--- inst/js/pinned.js
+++
@@ -1,6 +0,0 @@
-
-  $(document).ready(function(event) {
-    $(document).on("click", "div.standard_layout_output_panel", function(el) {
-        $(el.target).closest("div.standard_layout_output_panel").toggleClass("affix");
-    })
-  })
