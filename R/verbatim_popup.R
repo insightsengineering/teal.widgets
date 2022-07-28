b8ba3b75f8037f7ffa2ec2e726b3ef477c957412
Dawid Kałędkowski <dawid.kaledkowski@gmail.com>
2022-07-28 11:44:18 +0200

    Fix reactivity issue

--- R/verbatim_popup.R
+++ R/verbatim_popup.R
@@ -50,24 +50,16 @@ verbatim_popup_srv <- function(id, verbatim_content, title, style = FALSE, disab
   checkmate::assert_string(title)
   checkmate::assert_flag(style)
   checkmate::assert_class(disabled, classes = "reactive")
-  if (inherits(verbatim_content, "reactive")) {
-    shiny::validate(shiny::need(
-      checkmate::assert_multi_class(verbatim_content(), classes = c("expression", "character"))
-    ))
-  } else {
-    checkmate::assert_multi_class(verbatim_content, classes = c("expression", "character"))
-  }
-
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     disabled_flag_observer(disabled, ns("button"))
     modal_content <- format_content(verbatim_content, style)
     button_click_observer(
-      shiny::reactive(input$button),
-      ns("copy_button"),
-      ns("verbatim_content"),
-      title,
-      modal_content
+      click_event = shiny::reactive(input$button),
+      copy_button_id = ns("copy_button"),
+      copied_area_id = ns("verbatim_content"),
+      modal_title = title,
+      modal_content = modal_content
     )
   })
 }
@@ -149,11 +141,16 @@ button_click_observer <- function(click_event, copy_button_id, copied_area_id, m
 #' @return `reactive` with the formatted content
 format_content <- function(verbatim_content, style = FALSE) {
   shiny::reactive({
-    if (inherits(verbatim_content, "reactive")) {
-      content <- verbatim_content()
+    content <- if (inherits(verbatim_content, "reactive")) {
+      verbatim_content()
     } else {
-      content <- verbatim_content
+      verbatim_content
     }
+    shiny::validate(shiny::need(
+      checkmate::test_multi_class(content, classes = c("expression", "character")),
+      "verbatim_content should be an expression or a character"
+    ))
+
     content <- paste(as.character(content), collapse = "\n")
 
     if (style) {
