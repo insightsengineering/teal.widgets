029af5fe06961b8ff0a92a8411fa6c6e68a01616
Blazewim <marek.blazewicz@contractors.roche.com>
2022-09-28 15:50:37 +0200

    basic test added

--- inst/tws/tests/testthat/test-shinytest2.R
+++ inst/tws/tests/testthat/test-shinytest2.R
@@ -5,3 +5,17 @@ test_that("{shinytest2} recording: tws_screenshot", {
       height = 823, width = 1459)
   app$expect_screenshot()
 })
+
+
+test_that("{shinytest2} recording: tws_clicking", {
+  app <- AppDriver$new(variant = platform_variant(), name = "tws_download", height = 823,
+      width = 1459)
+  app$set_window_size(width = 1459, height = 766)
+  app$set_inputs(`table_with_settings-downbutton-file_format` = ".csv")
+  app$set_inputs(`table_with_settings-downbutton-file_format` = ".pdf")
+  app$click("table_with_settings-expand")
+  app$set_inputs(`table_with_settings-modal_downbutton-lpp` = 70)
+  app$set_inputs(`table_with_settings-modal_downbutton-file_format` = ".txt")
+  app$set_inputs(`table_with_settings-modal_downbutton-pagination_switch` = FALSE)
+  app$expect_screenshot()
+})
