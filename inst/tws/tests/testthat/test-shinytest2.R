9a2161e7328d9e138808f96d36eda1002b574b9f
Blazewim <marek.blazewicz@contractors.roche.com>
2022-10-03 17:56:11 +0200

    new tests

--- inst/tws/tests/testthat/test-shinytest2.R
+++ inst/tws/tests/testthat/test-shinytest2.R
@@ -1,6 +1,6 @@
 library(shinytest2)
 
-test_that("{shinytest2} recording: download", {
+test_that("file download", {
   app <- AppDriver$new(name = "tws_download", height = 671, width = 1178)
   app$click("ddb")
   app$set_inputs(ddb_state = TRUE)
@@ -18,7 +18,7 @@ test_that("{shinytest2} recording: download", {
 
 test_that("snapshotting inputs", {
   app <- AppDriver$new(name = "tws_filename")
-  file_name <- app$get_value(input = 'table_with_settings-downbutton-file_name')
+  file_name <- app$get_value(input = "table_with_settings-downbutton-file_name")
   file_name <- sub("_\\d{6}$", "", file_name)
   expected_file_name <- paste0("table_", strftime(Sys.Date(),
                                                   format = "%Y%m%d"))
