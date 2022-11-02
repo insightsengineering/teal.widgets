7b0e6af323bbe8fd91d38aeb37f41effb0c7c30f
Blazewim <marek.blazewicz@contractors.roche.com>
2022-11-02 11:26:54 +0100

    skip_on_ci

--- tests/testthat/test-table_with_settings.R
+++ tests/testthat/test-table_with_settings.R
@@ -2,6 +2,7 @@ library(shinytest2)
 
 test_that("table-with-settings", {
   skip_on_cran()
+  skip_on_ci()
 
   appdir <- system.file(package = "teal.widgets", "tws")
   shinytest2::test_app(appdir)
