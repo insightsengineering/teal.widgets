03e03dcda64334c8ac5d5eebe737ffaf85c14c41
Blazewim <marek.blazewicz@contractors.roche.com>
2022-11-09 15:29:54 +0100

    test reworked

--- inst/pws_click/tests/testthat/setup.R
+++ inst/pws_click/tests/testthat/setup.R
@@ -1,2 +1,4 @@
 # Load application support files into testing environment
 shinytest2::load_app_env()
+# Load application support files into testing environment
+shinytest2::load_app_env()
--- tests/testthat/test-plot_with_settings.R
+++ tests/testthat/test-plot_with_settings.R
@@ -64,7 +64,7 @@ testthat::test_that("if teal.plot_dpi is not an integer value then get_plot_dpi 
 
 test_that("table-with-settings", {
   skip_on_cran()
-
-  appdir <- system.file(package = "teal.widgets", "pws1")
+  skip_on_ci()
+  appdir <- system.file(package = "teal.widgets", "pws_click")
   shinytest2::test_app(appdir)
 })
