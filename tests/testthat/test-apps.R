1ec77574a2e967fdc0a4667ca1b9fc8776a0f309
Andrew Bates <andrew.bates@atorusresearch.com>
2022-11-15 13:06:31 -0800

    skip shinytest2 tests unless testing depth is set high enough

--- tests/testthat/test-apps.R
+++ tests/testthat/test-apps.R
@@ -1,4 +1,17 @@
 
+# tests the example apps in ./apps
+# each app lives in its own directory, specific to the purpose of the test(s)
+# the test specification lives under the app directory
+
+# example:
+# apps/pws_click_base tests plot with settings click functionality with a base R
+#   plot
+# pws_click_base/app.R is the app used for testing
+# pws_click_base/tests has the usual testthat file structure
+# pws_click_base/tests/testthat/test-shinytest2.R is the test specification
+
+
+skip_if_too_deep(5)
 
 lapply(
   list.files("apps", full.names = TRUE),
