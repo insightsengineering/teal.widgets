a05b91610fc1f520007ee221c30f0feffbdf7873
Mahmoud Hallal <mahmoud.hallal@roche.com>
2022-11-18 12:59:26 +0100

    styler

--- tests/testthat/apps/tws/tests/testthat/test-shinytest2.R
+++ tests/testthat/apps/tws/tests/testthat/test-shinytest2.R
@@ -5,23 +5,20 @@ app <- AppDriver$new(name = "tws")
 
 # Testing snapshots
 test_that("{shinytest2} file name and table content", {
-
   # default file name
   file_name <- app$get_value(input = "table_with_settings-downbutton-file_name")
   file_name <- sub("_\\d{6}$", "", file_name)
   expected_file_name <- paste0("table_", strftime(Sys.Date(),
-                                                  format = "%Y%m%d"
+    format = "%Y%m%d"
   ))
   testthat::expect_equal(file_name, expected_file_name)
 
   # check for table content
   app$expect_text("table")
-
 })
 
 # downloading tables
 test_that("{shinytest2} txt/csv download", {
-
   # download table in .csv
   app$click("table_with_settings-downbutton-dwnl")
   app$set_inputs(`table_with_settings-downbutton-dwnl_state` = TRUE)
@@ -35,7 +32,6 @@ test_that("{shinytest2} txt/csv download", {
   app$set_inputs(`table_with_settings-downbutton-dwnl_state` = TRUE)
   app$set_inputs(`table_with_settings-downbutton-file_name` = "tab2")
   app$expect_download("table_with_settings-downbutton-data_download")
-
 })
 
 # stop app for all tests
