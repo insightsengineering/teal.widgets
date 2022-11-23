dc66c6d206cbd48ed02759a5bc1dfd39b4c4181f
Blazewim <marek.blazewicz@contractors.roche.com>
2022-11-23 15:46:46 +0100

    reorganize

--- tests/testthat/test-apps.R
+++ tests/testthat/test-apps.R
@@ -8,8 +8,8 @@
 # apps/tws tests table with settings: manipulating and downloading
 
 # nolint start
-# skip_if_too_deep(5)
-skip_on_cran()
+skip_if_too_deep(5)
+# skip_on_cran()
 skip_on_ci()
 shinytest2::test_app(app_dir = "apps/tws")
 
