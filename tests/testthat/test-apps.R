321846cafd748fd0a8cd4324b2cf15683050eb6d
Blazewim <marek.blazewicz@contractors.roche.com>
2022-11-23 11:22:45 +0100

    remove all sht2 tests

--- tests/testthat/test-apps.R
+++ tests/testthat/test-apps.R
@@ -7,14 +7,24 @@
 # apps/pws_ggplot2 tests plot with settings click functionality with a ggplot2
 # apps/tws tests table with settings: manipulating and downloading
 
-
+# nolint start
 skip_if_too_deep(5)
 skip_on_cran()
 skip_on_ci()
 
-shinytest2::test_app(
-  app_dir = "tests/testthat/apps/tws", name = "app tests"
-)
-shinytest2::test_app(
-  app_dir = "tests/testthat/apps/pws_ggplot2", name = "app tests"
-)
+testthat::test_that("ggplot2_args, no error with empty inputs", {
+  testthat::expect_error(ggplot2_args(), NA)
+  testthat::expect_error(ggplot2_args(labs = list(), theme = list()), NA)
+  testthat::expect_error(ggplot2_args(labs = list()), NA)
+  testthat::expect_error(ggplot2_args(theme = list()), NA)
+  testthat::expect_error(ggplot2_args(list()), NA)
+  testthat::expect_error(ggplot2_args(list(), list()), NA)
+})
+
+# shinytest2::test_app(
+#   app_dir = "tests/testthat/apps/tws", name = "app tests"
+# )
+# shinytest2::test_app(
+#   app_dir = "tests/testthat/apps/pws_ggplot2", name = "app tests"
+# )
+# nolint end
