f48793c8594b33424f8bf2632d2c18adf722bfe7
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-12-02 11:23:11 +0100

    pws tests negative

--- tests/testthat/test-plot_with_settings.R
+++ tests/testthat/test-plot_with_settings.R
@@ -268,6 +268,82 @@ testthat::test_that("type_download_srv download a png file with NULL input dimen
   )
 })
 
+
+testthat::test_that("plot_with_settings_srv assert error", {
+  testthat::expect_error(
+    shiny::testServer(
+      teal.widgets:::plot_with_settings_srv,
+      args = list(id = "STH", plot_r = NULL),
+      expr = {}
+    ),
+    "Assertion"
+  )
+  testthat::expect_error(
+    shiny::testServer(
+      teal.widgets:::plot_with_settings_srv,
+      args = list(id = "STH", plot_r = function() plot(1), height = "20"),
+      expr = {}
+    ),
+    "Assertion"
+  )
+  testthat::expect_error(
+    shiny::testServer(
+      teal.widgets:::plot_with_settings_srv,
+      args = list(id = "STH", plot_r = function() plot(1), width = c(20, 100, 200)),
+      expr = {}
+    ),
+    "Assertion"
+  )
+  testthat::expect_error(
+    shiny::testServer(
+      teal.widgets:::plot_with_settings_srv,
+      args = list(id = "STH", plot_r = function() plot(1), show_hide_signal = TRUE),
+      expr = {}
+    ),
+    "Assertion"
+  )
+  testthat::expect_error(
+    shiny::testServer(
+      teal.widgets:::plot_with_settings_srv,
+      args = list(id = "STH", plot_r = function() plot(1), brushing = 20),
+      expr = {}
+    ),
+    "Assertion"
+  )
+  testthat::expect_error(
+    shiny::testServer(
+      teal.widgets:::plot_with_settings_srv,
+      args = list(id = "STH", plot_r = function() plot(1), clicking = 20),
+      expr = {}
+    ),
+    "Assertion"
+  )
+  testthat::expect_error(
+    shiny::testServer(
+      teal.widgets:::plot_with_settings_srv,
+      args = list(id = "STH", plot_r = function() plot(1), dblclicking = 20),
+      expr = {}
+    ),
+    "Assertion"
+  )
+  testthat::expect_error(
+    shiny::testServer(
+      teal.widgets:::plot_with_settings_srv,
+      args = list(id = "STH", plot_r = function() plot(1), dblclicking = 20),
+      expr = {}
+    ),
+    "Assertion"
+  )
+  testthat::expect_error(
+    shiny::testServer(
+      teal.widgets:::plot_with_settings_srv,
+      args = list(id = "STH", plot_r = function() plot(1), graph_align = "centery"),
+      expr = {}
+    ),
+    "Assertion"
+  )
+})
+
 plot_with_settings_args <- list(
   id = "STH",
   plot_r = NULL,
