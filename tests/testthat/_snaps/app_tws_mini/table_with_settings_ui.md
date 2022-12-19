c5df86d5556f6f87bf6d8fea51e8a177947171b2
Blazewim <marek.blazewicz@contractors.roche.com>
2022-12-19 15:18:33 +0100

    mini version updated

--- tests/testthat/_snaps/app_tws_mini/table_with_settings_ui.md
+++
@@ -1,4 +0,0 @@
-# mini version
-
-    "\n    \n      \n      B: Placebo\n      C: Combination\n    \n    \n      SEX\n      \n      \n    \n    \n      F\n      0\n      1\n    \n    \n      M\n      1\n      0\n    \n    \n      AGE\n      \n      \n    \n    \n      Mean\n      35.00\n      41.00\n    \n    \n        \n          \n        \n        \n      \n    \n  "
-
--- tests/testthat/test-table_with_settings_ui.R
+++ tests/testthat/test-table_with_settings_ui.R
@@ -1,92 +1,29 @@
 library(shinytest2)
 
-testthat::test_that("maxi version", {
+testthat::test_that("Table with settings: UI screenshots", {
   skip_on_cran()
   skip_on_ci()
   app <- AppDriver$new(
     app_tws(),
     name = "tws",
-    variant = "app_tws_max",
-    )
-  # check standard file naming
-  file_name <- app$get_value(input = "table_with_settings-downbutton-file_name")
-  testthat::expect_equal(
-    sub("_\\d{6}$", "", file_name),
-    paste0("table_", strftime(Sys.Date(), format = "%Y%m%d"))
+    variant = "app_tws_ui",
   )
-  app$expect_text("table")
-  app$set_window_size(width = 1392, height = 763)
 
-  # save txt (default)
-  app$click("table_with_settings-downbutton-dwnl")
-  app$set_inputs(`table_with_settings-downbutton-dwnl_state` = TRUE)
-  app$set_inputs(`table_with_settings-downbutton-file_name` = "table1")
-  app$expect_download("table_with_settings-downbutton-data_download")
-
-  # save csv:
-  app$set_inputs(`table_with_settings-downbutton-file_format` = ".csv")
-  app$set_inputs(`table_with_settings-downbutton-dwnl_state` = FALSE)
-  app$set_inputs(`table_with_settings-downbutton-file_name` = "table2")
-  app$expect_download("table_with_settings-downbutton-data_download")
-
-  # download using modal, txt first
-  app$click("table_with_settings-expand")
-  app$set_inputs(`table_with_settings-modal_downbutton-lpp` = 70)
-  app$click("table_with_settings-modal_downbutton-dwnl")
-  app$set_inputs(`table_with_settings-modal_downbutton-file_format` = ".txt")
-  app$set_inputs(`table_with_settings-modal_downbutton-pagination_switch` = FALSE)
-  app$click("table_with_settings-modal_downbutton-dwnl")
-  app$set_inputs(`table_with_settings-modal_downbutton-dwnl_state` = TRUE)
-  app$set_inputs(`table_with_settings-modal_downbutton-file_name` = "table3")
-  app$set_inputs(`table_with_settings-modal_downbutton-pagination_switch` = TRUE)
-  # test pagination for at least one case
-  app$set_inputs(`table_with_settings-modal_downbutton-lpp` = 1)
-  app$set_inputs(`table_with_settings-modal_downbutton-lpp` = 11)
-  app$expect_download("table_with_settings-modal_downbutton-data_download")
-
-  # download csv using modal:
-  app$set_inputs(`table_with_settings-modal_downbutton-file_format` = ".csv")
-  app$set_inputs(`table_with_settings-modal_downbutton-dwnl_state` = FALSE)
-  app$set_inputs(`table_with_settings-modal_downbutton-file_name` = "table4")
-  app$expect_download("table_with_settings-modal_downbutton-data_download")
-
-  # this compares the inputs and outputs stored in json:
-  app$expect_values()
-  # screenshot comparisons. Still experimenting whether there are values that will make it possible
-  app$expect_screenshot(threshold = 100, kernel_size = 15, delay = 0.5)
-  app$stop()
-})
-
-
-testthat::test_that("mini version", {
-  skip_on_cran()
-  skip_on_ci()
-  app <- AppDriver$new(
-    app_tws(),
-    name = "tws",
-    variant = "app_tws_mini",
-  )
-  file_name <- app$get_value(input = "table_with_settings-downbutton-file_name")
-  testthat::expect_equal(
-    sub("_\\d{6}$", "", file_name),
-    paste0("table_", strftime(Sys.Date(), format = "%Y%m%d"))
-  )
-  app$expect_text("table")
+  # snapshot of initialized app
+  app$expect_screenshot(threshold = 100, kernel_size = 10, delay = 0.5, name = "initial_view")
 
   # click on download button
   app$click("table_with_settings-downbutton-dwnl")
-  app$set_inputs(`table_with_settings-downbutton-file_name` = "table1")
 
-  # test clicking on modal, no download
+  # test clicking on modal
   app$click("table_with_settings-expand")
   app$click("table_with_settings-modal_downbutton-dwnl")
-  app$set_inputs(`table_with_settings-modal_downbutton-file_name` = "table2")
-  app$set_inputs(`table_with_settings-modal_downbutton-lpp` = 11)
+  app$set_inputs(`table_with_settings-modal_downbutton-file_name` = "table")
 
-  # and snapshots
-  app$expect_screenshot(threshold = 100, kernel_size = 15, delay = 0.5)
+  # snapshots of modal opened
+  app$expect_screenshot(threshold = 100, kernel_size = 10, delay = 0.5, name = "modal_view")
 
   # now test values in json
-  app$expect_values()
+  app$expect_values(screenshot_args = FALSE, name = "final_values")
   app$stop()
 })
