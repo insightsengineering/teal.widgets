3cc257f8e90949e10276fddeb70320ae23e9e6c3
kartikeya <kartikeya.kirar@unicle.life>
2023-08-07 18:01:26 +0530

    adding unit test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,27 @@
 testthat::test_that("get_bs_version", {
   testthat::expect_identical(get_bs_version(), "3")
 })
+
+test_that("apply_plot_modifications correctly modifies plot objects", {
+  # Create a sample ggplot2 plot object
+  ggplot_obj <- ggplot2::ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) +
+    geom_point()
+
+  # Test case 1: Modify ggplot object with dblclicking enabled
+  modified_ggplot <- apply_plot_modifications(ggplot_obj, "gg", TRUE, list(x = c(4, 7), y = c(2, 4)))
+  expect_true(identical(class(modified_ggplot), class(ggplot_obj)))  # Check class of modified object
+
+  # Test case 2: Modify ggplot object with dblclicking disabled
+  modified_ggplot <- apply_plot_modifications(ggplot_obj, "gg", FALSE, list(x = c(4, 7), y = c(2, 4)))
+  expect_true(identical(class(modified_ggplot), class(ggplot_obj)))  # Check class of modified object
+
+  # Test case 3: Modify grob object
+  grob_obj <- grid::rectGrob()
+  modified_grob <- apply_plot_modifications(grob_obj, "grob", TRUE, list(x = c(0, 1), y = c(0, 1)))
+  expect_null(modified_grob)
+
+  # Test case 4: Do not modify plot object
+  unchanged_plot <- apply_plot_modifications(ggplot_obj, "other_type", TRUE, list(x = c(4, 7), y = c(2, 4)))
+  expect_true(identical(unchanged_plot, ggplot_obj))  # Check if plot object remains unchanged
+})
+
