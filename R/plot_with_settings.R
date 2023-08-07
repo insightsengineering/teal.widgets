bebefa0f702b349960c95884516b7720a18a0fde
kartikeya <kartikeya.kirar@unicle.life>
2023-08-07 16:39:44 +0530

    fixing the grob rendering onb modal

--- R/plot_with_settings.R
+++ R/plot_with_settings.R
@@ -364,11 +364,6 @@ plot_with_settings_srv <- function(id,
       if (plot_type() == "gg" && dblclicking) {
         plot_r() +
           ggplot2::coord_cartesian(xlim = ranges$x, ylim = ranges$y, expand = FALSE)
-      } else if (plot_type() == "grob") {
-        # calling grid.draw on plot_r() is needed;
-        # otherwise the plot will not re-render if the user triggers the zoom in or out feature of the browser.
-        grid::grid.newpage()
-        grid::grid.draw(plot_r())
       } else {
         plot_r()
       }
@@ -376,15 +371,27 @@ plot_with_settings_srv <- function(id,
 
     p_height <- reactive(`if`(!is.null(input$height), input$height, height[1]))
     p_width <- reactive(`if`(!is.null(input$width), input$width, default_slider_width()[1]))
-    output$plot_main <- renderPlot(
-      plot_reactive(),
+    output$plot_main <- renderPlot({
+      if (plot_type() == "grob") {
+        grid::grid.newpage()
+        grid::grid.draw(plot_reactive())
+      } else {
+        plot_reactive()
+      }},
       res = get_plot_dpi(),
       height = p_height,
       width = p_width
     )
 
-    output$plot_modal <- renderPlot(
-      plot_reactive(),
+    output$plot_modal <- renderPlot({
+      if (plot_type() == "grob") {
+        # calling grid.draw on plot_r() is needed in renderPlot;
+        # otherwise the plot will not re-render if the user triggers the zoom in or out feature of the browser.
+        grid::grid.newpage()
+        grid::grid.draw(plot_reactive())
+      } else {
+        plot_reactive()
+      }},
       res = get_plot_dpi(),
       height = reactive(input$height_in_modal),
       width = reactive(input$width_in_modal)
