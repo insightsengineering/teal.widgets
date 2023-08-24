9e51e675e1e1c04a9f68de93e51a5163800869c9
kartikeya kirar <kirar.kartikeya1@gmail.com>
2023-08-24 17:43:17 +0530

    543 fixing Plot setting panel icon (#181)
    this fixes
    https://github.com/insightsengineering/teal.modules.general/issues/543
    
    Here I have added Jquery to toggle icon and added class card-heading to
    identify target div() bs4/bs5 version.
    
    ---------
    
    Co-authored-by: kartikeya <kartikeya.kirar@unicle.life>

---
+++ inst/js/panel_group.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(document).on('click', '.panel-heading, .card-heading', function() {
+    var $icon = $(this).find('.dropdown-icon');
+
+    if ($(this).hasClass('collapsed')) {
+      $icon.removeClass('fa-angle-down').addClass('fa-angle-right');
+      $(this).attr('aria-expanded', 'true');
+    } else {
+      $icon.removeClass('fa-angle-right').addClass('fa-angle-down');
+      $(this).attr('aria-expanded', 'false');
+    }
+  });
+});
