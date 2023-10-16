6b917aed967c7fccd49cb3f333c66e491a4c59f8
vedhav <vedhaviyash4@gmail.com>
2023-10-16 19:25:03 +0530

    chore: replace qplot with ggplot equivalent

--- R/plot_with_settings.R
+++ R/plot_with_settings.R
@@ -119,7 +119,8 @@ plot_with_settings_ui <- function(id) {
 #'   ),
 #'   server = function(input, output, session) {
 #'     plot_r <- reactive({
-#'       ggplot2::qplot(x = 1, y = 1)
+#'       ggplot2::ggplot(NULL) +
+#'         ggplot2::geom_point(ggplot2::aes(x = 1, y = 1))
 #'     })
 #'
 #'     plot_with_settings_srv(
@@ -191,7 +192,8 @@ plot_with_settings_ui <- function(id) {
 #'   ),
 #'   server = function(input, output, session) {
 #'     plot_r <- reactive({
-#'       ggplot2::qplot(x = 1:5, y = 1:5)
+#'       ggplot2::ggplot(NULL) +
+#'         ggplot2::geom_point(ggplot2::aes(x = 1:5, y = 1:5))
 #'     })
 #'
 #'     plot_data <- plot_with_settings_srv(
@@ -227,7 +229,13 @@ plot_with_settings_ui <- function(id) {
 #'     )
 #'   ),
 #'   server = function(input, output, session) {
-#'     plot_r <- reactive(ggplot2::qplot(data = faithful, x = waiting, y = eruptions))
+#'     plot_r <- plot_r <- reactive(
+#'       ggplot2::ggplot(
+#'         data = faithful,
+#'         ggplot2::aes(x = waiting, y = eruptions)
+#'       ) +
+#'         ggplot2::geom_point()
+#'     )
 #'
 #'     show_hide_signal_rv <- reactiveVal(TRUE)
 #'
