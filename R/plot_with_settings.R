b83105eeefe6ba76ea9d35a5374469e01c5f87c6
Vedha Viyash <49812166+vedhav@users.noreply.github.com>
2023-10-16 21:41:57 +0530

    Update R/plot_with_settings.R
    Co-authored-by: Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>
    Signed-off-by: Vedha Viyash <49812166+vedhav@users.noreply.github.com>

--- R/plot_with_settings.R
+++ R/plot_with_settings.R
@@ -119,8 +119,7 @@ plot_with_settings_ui <- function(id) {
 #'   ),
 #'   server = function(input, output, session) {
 #'     plot_r <- reactive({
-#'       ggplot2::ggplot(NULL) +
-#'         ggplot2::geom_point(ggplot2::aes(x = 1, y = 1))
+#'       ggplot2::ggplot(faithful, ggplot2::aes(x = waiting, y = eruptions)) + ggplot2::geom_point()
 #'     })
 #'
 #'     plot_with_settings_srv(
