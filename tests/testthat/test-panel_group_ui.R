31e1c5bda407b09c0bdbd2c28801f0b001684df0
m7pr <marcin.kosinski.mk1@roche.com>
2024-04-16 12:39:42 +0200

    test for panel_group and panel_item

---
+++ tests/testthat/test-panel_group_ui.R
@@ -0,0 +1,58 @@
+testthat::test_that("e2e - panel_group: clicking plot settings accordion panel shows plot setting", {
+  skip_if_too_deep(5)
+
+  app <- shinytest2::AppDriver$new(
+    app_driver_panel_group(),
+    name = "panel_group",
+    variant = "app_driver_panel_group",
+  )
+
+  app$wait_for_idle(timeout = default_idle_timeout)
+
+  is_visible <- function(element) {
+    any(
+      unlist(
+        app$get_js(
+          sprintf(
+            "Array.from(document.querySelectorAll('%s')).map(el => el.checkVisibility())",
+            element
+          )
+        )
+      )
+    )
+  }
+
+  testthat::expect_false(is_visible("#size"))
+  testthat::expect_false(is_visible("#alpha"))
+  testthat::expect_false(is_visible("#ggtheme-label"))
+
+  # After click they are visible.
+  app$click(selector = "#_div > div.panel-heading.collapsed")
+  testthat::expect_true(is_visible("#size"))
+  testthat::expect_true(is_visible("#alpha"))
+  testthat::expect_true(is_visible("#ggtheme-label"))
+
+  app$stop()
+})
+
+testthat::test_that("e2e - panel_group: plot settings have default values", {
+  skip_if_too_deep(5)
+
+  app <- shinytest2::AppDriver$new(
+    app_driver_panel_group(),
+    name = "panel_group",
+    variant = "app_driver_panel_group",
+  )
+
+  app$wait_for_idle(timeout = default_idle_timeout)
+
+  app$click(selector = "#_div > div.panel-heading.collapsed")
+
+  init_vals <- app$get_values()
+
+  testthat::expect_identical(init_vals$input$alpha, 1L)
+  testthat::expect_identical(init_vals$input$size, 2L)
+  testthat::expect_identical(init_vals$input$ggtheme, "default")
+
+  app$stop()
+})
