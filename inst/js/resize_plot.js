3373f315a4347444a81ea6ed41450707c9d864b2
Aleksander Chlebowski <aleksander.chlebowski@sanofi.com>
2024-07-02 11:55:06 +0200

    add JS for resizing plots

---
+++ inst/js/resize_plot.js
@@ -0,0 +1,24 @@
+//
+var establishPlotResizing = function (
+  plot_out_main,
+  flex_width,
+  plot_modal_width) {
+
+  // Create resize observer to trigger shiny actions when plot container is resized.
+  var plotObserver = new ResizeObserver(function () {
+    Shiny.onInputChange(flex_width, document.getElementById(plot_out_main).clientWidth);
+    Shiny.onInputChange(plot_modal_width, 0.87 * window.innerWidth);
+    //based on modal CSS property, also accounting for margins
+  });
+
+  // Create mutation observer to delay activation of resize observer.
+  // Activating observer before the target element exists is impossible.
+  var plotNoticer = new MutationObserver(function () {
+    const plotContainer = document.getElementById(plot_out_main);
+    if (plotContainer === null) return;
+    plotObserver.observe(plotContainer, { box: "border-box" } );
+    plotNoticer.disconnect();
+  });
+
+  plotNoticer.observe(document, { subtree: true, childList: true });
+};
