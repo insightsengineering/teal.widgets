bd5d12bc6fb0995eca59f2b9f9f463c184579eec
Lluís Revilla <lluis.revilla_sancho@roche.com>
2025-06-19 11:54:58 +0200

    Update examples and code

--- R/get_dt_rows.R
+++ R/get_dt_rows.R
@@ -16,29 +16,30 @@
 #' ui <- function(id) {
 #'   ns <- NS(id)
 #'   tagList(
-#'     DTOutput(ns("data_table")),
-#'     get_dt_rows(ns("data_table"), ns("dt_rows"))
+#'     get_dt_rows(ns("data_table"), ns("dt_rows")),
+#'     textOutput(ns("rows")),
+#'     DT::DTOutput(ns("data_table"))
 #'   )
 #' }
 #'
 #' # use the input$dt_rows in the Shiny Server function
 #' server <- function(id) {
 #'   moduleServer(id, function(input, output, session) {
-#'     output$data_table <- renderDataTable(
-#'       {
-#'         iris
-#'       },
-#'       options = list(pageLength = input$dt_rows)
-#'     )
+#'
+#'     output$data_table <- DT::renderDataTable(iris)
+#'     # Change rows selected to see the first line on the UI change
+#'     rows <- reactive({
+#'       paste0("Selected Rows ", input$dt_rows)
+#'       })
+#'     output$rows <- renderText(rows())
+#'
 #'   })
 #' }
 #'
-#' if (interactive()) {
-#'   shinyApp(
-#'     ui = ui("my_table_module"),
-#'     server = function(input, output, session) server("my_table_module")
-#'   )
-#' }
+#' shinyApp(
+#'   ui = ui("my_table_module"),
+#'   server = function(input, output, session) server("my_table_module")
+#' )
 #' @export
 get_dt_rows <- function(dt_name, dt_rows) {
   tags$head(
@@ -46,7 +47,7 @@ get_dt_rows <- function(dt_name, dt_rows) {
       sprintf(
         "$(document).ready(function() {
         $('%s').on('length.dt', function(e, settings, len) {
-        Shiny.onInputChange('%s', len);
+        Shiny.setInputValue('%s', len);
         });
         });",
         paste0("#", dt_name),
