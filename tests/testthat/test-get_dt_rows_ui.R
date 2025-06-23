558438c4fbcfa936a41ae143c6ecfce69e328124
Lluís Revilla <lluis.revilla_sancho@roche.com>
2025-06-23 13:29:36 +0200

    Adding interactivity on the DT table

--- tests/testthat/test-get_dt_rows_ui.R
+++ tests/testthat/test-get_dt_rows_ui.R
@@ -12,7 +12,22 @@ app_driver_gdr <- function() {
   # use the input$dt_rows in the Shiny Server function
   server <- function(id) {
     moduleServer(id, function(input, output, session) {
-      output$data_table <- DT::renderDataTable(iris)
+      output$data_table <- DT::renderDataTable(
+        iris,
+        extensions = 'Buttons',
+        options = list(
+          dom = 'Bfrtip',
+          buttons = list(
+            list(
+              extend = "collection",
+              text = 'Show 25',
+              attr = list(id = 'show_25'),
+              action = DT::JS(
+                "function ( e, dt, node, config ) { dt.page.len(25); dt.ajax.reload(); }")
+            )
+          )
+        )
+      )
       rows <- reactive({
         paste0("Selected Rows ", input$dt_rows)
       })
@@ -41,13 +56,16 @@ testthat::test_that(
     # Check initial value
     dt_state <- app_driver$get_values(input = "my_table_module-data_table_state")
     testthat::expect_equal(dt_state$input$`my_table_module-data_table_state`$length, 10)
+    testthat::expect_equal(app_driver$get_value(input = "my_table_module-dt_rows"), NULL)
 
     # Change rows value
-    # FIXME: Couldn't find a way to programatically change that
+    app_driver$click(selector = "#show_25")
+    app_driver$wait_for_idle(timeout = default_idle_timeout)
 
     # Check value again
     dt_state <- app_driver$get_values(input = "my_table_module-data_table_state")
-    testthat::expect_equal(dt_state$input$`my_table_module-data_table_state`$length, 10)
+    testthat::expect_equal(dt_state$input$`my_table_module-data_table_state`$length, 25)
+    testthat::expect_equal(app_driver$get_value(input = "my_table_module-dt_rows"), 25)
 
     app_driver$stop()
   }
