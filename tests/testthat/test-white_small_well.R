26a7ce657f8dd014ba8f519027c1f07174e4b3c7
osenan <oriol.senan@businesspartner.roche.com>
2025-11-10 10:36:47 -0300

    tests: add test for white small well

---
+++ tests/testthat/test-white_small_well.R
@@ -0,0 +1,10 @@
+testthat::test_that("Check that the class for white_small_well is correct", {
+  dummy_white_small_well <- white_small_well()
+  expected_class <- "shiny.tag.list"
+  testthat::expect_s3_class(dummy_white_small_well, expected_class)
+})
+
+testthat::test_that("Snapshot test for white_small_well", {
+  testthat::local_edition(3)
+  testthat::expect_snapshot(white_small_well())
+})
