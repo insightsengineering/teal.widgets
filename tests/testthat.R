6b78aacb92b1aadc552196f79d3e5f905c9f78c0
osenan <oriol.senan@businesspartner.roche.com>
2025-11-09 21:36:02 -0300

    fix: add conditional statement for running tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 pkg_name <- "teal.widgets"
 library(pkg_name, character.only = TRUE)
 testthat::test_check(pkg_name)
-library(testthat)
+if (!is.null(requireNamespace("testthat"))) library(testthat) else message("Please install testthat to run tests")
