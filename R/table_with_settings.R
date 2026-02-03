e804de5ade96bb2719491328e09c40dfe5a11e1b
Marcin <133694481+m7pr@users.noreply.github.com>
2026-02-03 17:27:18 +0100

    Fix output assignment for table rendering
    Looks like this output is not used anywhere
    
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- R/table_with_settings.R
+++ R/table_with_settings.R
@@ -111,7 +111,7 @@ table_with_settings_srv <- function(id, table_r, show_hide_signal = reactive(TRU
       }
     })
 
-    output$table_out_main <- output$table_out_modal <- renderUI({
+    output$table_out_main <- renderUI({
       rtables::as_html(table_r())
     })
 
